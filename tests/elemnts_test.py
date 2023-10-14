c37d8165959d9137353d9f2f6fde3be538cdd848
CytoClovn <nickolayknz@gmail.com>
2023-10-14 15:37:03 +0300

    fixed arguments in go_to_element update elements tests added test checkboxes

--- tests/elemnts_test.py
+++ tests/elemnts_test.py
@@ -1,5 +1,8 @@
+import time
+
 from conftest import driver
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
+
 
 class TestElements:
     class TestTextBox:
@@ -13,5 +16,26 @@ class TestElements:
             assert current_address == output_cut_addr, 'Current does not match'
             assert permanent_address == output_perm_addr, 'Permanent does not match'
 
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            # input_checkbox_new = []
+            # output_result_new = []
+            # for i in input_checkbox:
+            #    a = i.replace(' ', '')
+            #    a = a.replace('.doc', '')
+            #    input_checkbox_new.append(a.lower())
+            # for i in output_result:
+            #    output_result_new.append(i.lower())
+            # time.sleep(3)
+            # print(input_checkbox_new)
+            # print(output_result_new)
+            # assert output_result_new == input_checkbox_new
+            assert input_checkbox == output_result, 'checkboxes have not been selected'
 
 
